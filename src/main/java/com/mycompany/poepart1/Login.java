3006027519f9dbe164a187b4d026b025a0929440
RC_Student_lab <RC_Student_lab@10.0.0.20>
2024-09-25 08:54:20 +0000

    second commit

--- src/main/java/com/mycompany/poepart1/Login.java
+++ src/main/java/com/mycompany/poepart1/Login.java
@@ -9,5 +9,31 @@ package com.mycompany.poepart1;
  * @author RC_Student_lab
  */
 public class Login {
+   //used the private access modifier to declare my variables
+    private String name;
+    private String surname;
+    private String password;
+    private String username;
+    //used public access modifier to declare my boolean variable so that i can be to use then in other methods when comparing
+    public  boolean correct=true;
+    public boolean verify=false;
+    public boolean check=false;
+    //used getters to return my variables
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
     
+    //meethod that captures the user detatils when they are registering  
 }
