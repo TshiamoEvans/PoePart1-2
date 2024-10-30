ad8634491c5610bd467ebc5ab22c3b9815b4a96a
RC_Student_lab <RC_Student_lab@10.0.0.20>
2024-10-30 18:25:06 +0000

    1st commit

--- src/main/java/com/mycompany/poepartoneandtwo/Task.java
+++ src/main/java/com/mycompany/poepartoneandtwo/Task.java
@@ -23,35 +23,3 @@ public class Task {
         
         return check;
     }
-    public String CreateTaskID(String taskname , int tasknumber , String Developerdetails){
-        
-        String taskid = taskname.substring(0,2) + ":" + tasknumber + ":" + Developerdetails.substring(Developerdetails.length()-3);
-        
-        
-        return taskid.toUpperCase(); 
-        
-    }
-   public String printTaskDetails(String taskdetails ,  String developerDetails,int tasknumber, String taskname ,String taskdiscript, String taskid, int taskduration  ){
-         // Declared and assigned a variable which will cadd all the task details
-         String values = "*****The task details are as follows***** \n" + "Tasksatus: " + taskdetails+ "\n"
-                 + "Developer details: " + developerDetails + "\n"
-                + "Task number: " + tasknumber + "\n" 
-                 + "Task name: " + taskname + "\n"
-                 + "Task description: " + taskdiscript + "\n" 
-                 + "Task id:  " + taskid + "\n" +
-                 "Task duration: " +taskduration + "hrs" + " \n <<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>> \n " ;
-         
-         // used JOption to display the task details
-         JOptionPane.showMessageDialog(null,values);
-         
-         return "";
-     }
-     // created a method to calculate the total hours of all the tasks combined, passed on a parameter that is a integer
-     public int returnHours(int hours){
-         //declared and assigned a variable to the parameter
-         int totalhours = hours;
-     
-     
-     return totalhours;
-     } 
-}
